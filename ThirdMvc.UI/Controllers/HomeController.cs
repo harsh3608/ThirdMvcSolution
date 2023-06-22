85076675f688b04526f6f872e44632c3e6bae87e
Harsh Patel <harsh3608@gmail.com>
2023-06-22 13:02:56 +0530

    add images in webroot folder and changes in styles

--- ThirdMvc.UI/Controllers/HomeController.cs
+++ ThirdMvc.UI/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace ThirdMvc.UI.Controllers
             return View();
         }
 
+        public IActionResult Authors()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
